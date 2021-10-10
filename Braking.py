1a8cc8e0aec99743fa1caa3886c2f9d4a9f1a7c2
NeilRoy678 <74729526+NeilRoy678@users.noreply.github.com>
2021-10-10 13:56:27 +0530

    Update Braking.py

--- Braking.py
+++ Braking.py
@@ -1,15 +1,12 @@
 """my_controller controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
+
 from controller import Robot, DistanceSensor, Motor,Brake
 from vehicle import Driver
 import math
 # create the Robot instance.
 if __name__ == "__main__":
     driver = Driver()
-    #robot = Robot()
-    #driver.setSteeringAngle(0.2)
     driver.setCruisingSpeed(20)
 
     ps = []
@@ -34,15 +31,9 @@ if __name__ == "__main__":
     # detect obstacles
         right_obstacle = psValues[0] <17.0 or psValues[1] > 80.0 or psValues[2] > 80.0
         left_obstacle = psValues[4] > 80.0 or psValues[5] > 80.0 or psValues[6] > 80.0
+        #Performs Braking When obstacle is detected in front 
         if psValues[0]<18:
             driver.setBrakeIntensity(1)
-            #driver.setSteeringAngle(-0.4)
 
             driver.setCruisingSpeed(0)
             
-        #driver.setSteeringAngle(0.5)
-             
-            #print(driver.getCurrentSpeed)    
-        
-        #angle = 0.3 * math.cos(driver.getTime())
-        #driver.setSteeringAngle(angle)
